50a05767d5578b51d9cf2aa7db002ad2ec463a72
Felipe Melo <felipemelogomes36@gmail.com>
2024-05-17 13:33:46 -0300

    feat: add script migrations package json

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,5 +3,13 @@ import { DataSource } from "typeorm";
 export const AppDataSource = new DataSource({
     type: "sqlite",
     database: "src/database/db.sqlite",
-    migrations: ["src/database/migrations/*.js"],
+    migrations: ["src/database/migrations/*.ts"],
 });
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Conectado ao banco de dados");
+    })
+    .catch((error) => {
+        console.log(error);
+    });
